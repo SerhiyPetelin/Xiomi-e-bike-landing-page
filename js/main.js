f9debf717e9e86a6f0841164ef3a65bd5bd6f56c
SerhiyPetelin <serhiypetelin@gmail.com>
2023-01-15 11:50:44 +0200

    Finish with whole layout

--- js/main.js
+++ js/main.js
@@ -8,3 +8,10 @@ $(function () {
     });
 
 });
+
+$(".menu, .bike__column").on("click","a", function (event) {
+    event.preventDefault();
+    var id  = $(this).attr('href'),
+        top = $(id).offset().top;
+    $('body,html').animate({scrollTop: top}, 1500);
+});
